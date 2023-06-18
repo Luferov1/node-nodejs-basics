b6f46a0b6419efeb937d47f7c684af03aa634ae2
Luferov1 <ldima12022002@gmail.com>
2023-06-18 18:59:30 +0300

    feat: add worker threads solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+import getPath from '../utils/functions/getPath.js';
+
 const performCalculations = async () => {
-    // Write your code here
+  const numberOfCPUs = cpus().length;
+  const workerPath = getPath(import.meta.url, '/worker.js');
+  const promisesArr = [];
+  for (let i = 0; i < numberOfCPUs; i += 1) {
+    const promise = new Promise((resolve, reject) => {
+      const worker = new Worker(workerPath, {
+        workerData: i + 10,
+      });
+      worker.on('message', (msg) => resolve(msg));
+      worker.on('error', (mgs) => reject(mgs));
+    })
+    promisesArr.push(promise);
+  }
+  const settledPromises = await Promise.allSettled(promisesArr);
+  const result = settledPromises
+    .map(
+      (result) => result.status === 'fulfilled'
+        ? { status: 'resolved', data: result.value }
+        : { status: 'error', data: null }
+    );
+  console.log(result);
 };
 
 await performCalculations();
